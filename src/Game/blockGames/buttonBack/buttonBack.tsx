22776df7d7c33ec4548b970cd1d99452d3e64d63
MaxHuzarevich <pinsk.maks@mail.ru>
2022-06-02 20:20:05 +0300

    - add type for buttonBack (route)

--- src/Game/blockGames/buttonBack/buttonBack.tsx
+++ src/Game/blockGames/buttonBack/buttonBack.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import {NavLink} from "react-router-dom";
 import './buttonBackStyle.css'
+import useSound from "use-sound";
+import soundButtonBack from '../../../assets/audio/backButtonClick.mp3'
 
-export const ButtonBack = () => {
+type routeType = {
+    to: string
+}
+
+export const ButtonBack = (props:routeType) => {
+
+    const [play, {stop}] = useSound(soundButtonBack)
+
     return (
         <div>
-            <NavLink to='/' className='back'>
+            <NavLink to={props.to}
+                     className='back'
+                     onMouseEnter={() => play()}
+                     onMouseLeave={() => stop()}>
                 <div>BACK</div>
             </NavLink>
         </div>
